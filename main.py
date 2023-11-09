befdaa69692c44cc2215e9c6d56759d02480dce8
D Shrat <dshrat29@gmail.com>
2023-11-09 17:02:29 +0700

    hisk hisk

--- main.py
+++ main.py
@@ -13,27 +13,22 @@ import cv2
 from imgaug import augmenters as iaa
 import os
 import aiofiles
+import uvicorn
+from uvicorn import run
+
 
 # webserver code start
 app = FastAPI()
 # webserver code end
 
-origins = ["http://localhost:3000"]
+origins = ["http://localhost:3000"]  
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["POST", "GET", "OPTIONS", "PUT", "DELETE", "HEAD"],
     allow_headers=[
-        "custId",
-        "appId",
-        "Origin",
-        "Content-Type",
-        "Cookie",
-        "X-CSRF-TOKEN",
-        "Accept",
-        "Authorization",
-        "X-XSRF-TOKEN",
-        "Access-Control-Allow-Origin",
+        "custId", "appId", "Origin", "Content-Type", "Cookie", "X-CSRF-TOKEN",
+        "Accept", "Authorization", "X-XSRF-TOKEN", "Access-Control-Allow-Origin",
         "X-Requested-With",
     ],
     expose_headers=["Authorization", "authenticated"],
@@ -42,28 +37,31 @@ app.add_middleware(
 )
 
 # AI code
-resnet50 = tf.keras.applications.resnet50.ResNet50(
-    include_top=False, weights="imagenet"
-)
-modelR = tf.keras.Model(
-    inputs=resnet50.input, outputs=resnet50.get_layer("conv4_block5_out").output
-)
+resnet50 = tf.keras.applications.resnet50.ResNet50(include_top=False, weights='imagenet')
+modelR = tf.keras.Model(inputs=resnet50.input, outputs=resnet50.get_layer('conv4_block5_out').output)
 
-xception = tf.keras.applications.xception.Xception(
-    include_top=False, weights="imagenet"
-)
-modelX = tf.keras.Model(
-    inputs=xception.input, outputs=xception.get_layer("conv2d_2").output
-)
+xception = tf.keras.applications.xception.Xception(include_top=False, weights='imagenet')
+modelX = tf.keras.Model(inputs=xception.input, outputs=xception.get_layer('conv2d_2').output)
 
-vgg19 = tf.keras.applications.vgg19.VGG19(include_top=False, weights="imagenet")
-modelV = tf.keras.Model(
-    inputs=vgg19.input, outputs=vgg19.get_layer("block5_conv4").output
-)
+vgg19 = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
+modelV = tf.keras.Model(inputs=vgg19.input, outputs=vgg19.get_layer('block5_conv4').output)
 
-checkpoint_path_R = "./resnet/my_capsule_network"
-checkpoint_path_X = "./xception/my_capsule_network"
-checkpoint_path_V = "./vgg/my_capsule_network"
+checkpoint_path_R = "./resnet/my_capsule_network"  
+checkpoint_path_X = "./xception/my_capsule_network"  
+checkpoint_path_V = "./vgg/my_capsule_network"  
+
+
+def detect_face(image_path):
+    image = cv2.imread(image_path)
+    face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    if len(faces) == 0:
+        return False  # No faces detected
+    else:
+        return True  # Faces detected
+
 
 
 def preprocess_image(img):
@@ -71,11 +69,9 @@ def preprocess_image(img):
     img_array = np.array(img)
 
     # Define augmentation sequence
-    augmenter = iaa.Sequential(
-        [
-            iaa.AdditiveGaussianNoise(scale=(0, 0.05 * 255)),
-        ]
-    )
+    augmenter = iaa.Sequential([
+        iaa.AdditiveGaussianNoise(scale=(0, 0.05 * 255)), 
+    ])
 
     # # Apply the augmentation to the image
     img_array = augmenter.augment_image(img_array)
@@ -87,8 +83,7 @@ def preprocess_image(img):
 
     return img_array
 
-
-# RESNET50
+#RESNET50
 def try_predict_resnet(filepath):
     image = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
     image = np.stack((image, image, image), axis=-1)
@@ -96,19 +91,17 @@ def try_predict_resnet(filepath):
     image = np.expand_dims(image, axis=0)
     image = tf.keras.applications.resnet50.preprocess_input(image)
     features = modelR.predict(image)
-    combined_feature = np.mean(features, axis=-1, keepdims=True)  # 1 layer
-
+    combined_feature = np.mean(features, axis=-1, keepdims=True) #1 layer
+    
     with tf.compat.v1.Session() as sess:
-        saver = tf.compat.v1.train.import_meta_graph(checkpoint_path_R + ".meta")
+        saver = tf.compat.v1.train.import_meta_graph(checkpoint_path_R + '.meta')
         saver.restore(sess, checkpoint_path_R)
         graph = tf.compat.v1.get_default_graph()
         X = graph.get_tensor_by_name("X:0")  # Sesuaikan dengan nama placeholder
 
         # prediksi pada data X_test
-        predictions = graph.get_tensor_by_name("y_pred:0")  # Ganti nama tensor output
-        predicted_labels = sess.run(
-            predictions, feed_dict={X: combined_feature.reshape(1, 14, 14, 1)}
-        )
+        predictions = graph.get_tensor_by_name("y_pred:0")  # Ganti nama tensor output 
+        predicted_labels = sess.run(predictions, feed_dict={X: combined_feature.reshape(1, 14, 14, 1)})
 
     # predicted_labels sekarang akan berisi hasil prediksi untuk data X_test
     # return {str(predicted_labels)}
@@ -118,28 +111,25 @@ def try_predict_resnet(filepath):
         return {"deepfake"}
     else:
         return {"gagal mendeteksi label, hasilnya ": predictions}
-
-
-# VGG19
+    
+#VGG19
 def try_predict_vgg(filepath):
     image = cv2.imread(filepath)
     image = preprocess_image(image)
     image = np.expand_dims(image, axis=0)
     image = tf.keras.applications.vgg19.preprocess_input(image)
     features = modelV.predict(image)
-    combined_feature = np.mean(features, axis=-1, keepdims=True)  # 1 layer
-
+    combined_feature = np.mean(features, axis=-1, keepdims=True) #1 layer
+    
     with tf.compat.v1.Session() as sess:
-        saver = tf.compat.v1.train.import_meta_graph(checkpoint_path_V + ".meta")
+        saver = tf.compat.v1.train.import_meta_graph(checkpoint_path_V + '.meta')
         saver.restore(sess, checkpoint_path_V)
         graph = tf.compat.v1.get_default_graph()
         X = graph.get_tensor_by_name("X:0")  # Sesuaikan dengan nama placeholder
 
         # prediksi pada data X_test
-        predictions = graph.get_tensor_by_name("y_pred:0")  # Ganti nama tensor output
-        predicted_labels = sess.run(
-            predictions, feed_dict={X: combined_feature.reshape(1, 14, 14, 1)}
-        )
+        predictions = graph.get_tensor_by_name("y_pred:0")  # Ganti nama tensor output 
+        predicted_labels = sess.run(predictions, feed_dict={X: combined_feature.reshape(1, 14, 14, 1)})
 
     # predicted_labels sekarang akan berisi hasil prediksi untuk data X_test
     if predicted_labels == 0:
@@ -148,28 +138,25 @@ def try_predict_vgg(filepath):
         return {"deepfake"}
     else:
         return {"gagal mendeteksi label, hasilnya ": predictions}
-
-
-# XCEPTION
+    
+#XCEPTION
 def try_predict_xception(filepath):
     image = cv2.imread(filepath)
     image = preprocess_image(image)
     image = np.expand_dims(image, axis=0)
     image = tf.keras.applications.xception.preprocess_input(image)
     features = modelX.predict(image)
-    combined_feature = np.mean(features, axis=-1, keepdims=True)  # 1 layer
-
+    combined_feature = np.mean(features, axis=-1, keepdims=True) #1 layer
+    
     with tf.compat.v1.Session() as sess:
-        saver = tf.compat.v1.train.import_meta_graph(checkpoint_path_X + ".meta")
+        saver = tf.compat.v1.train.import_meta_graph(checkpoint_path_X + '.meta')
         saver.restore(sess, checkpoint_path_X)
         graph = tf.compat.v1.get_default_graph()
         X = graph.get_tensor_by_name("X:0")  # Sesuaikan dengan nama placeholder
 
         # prediksi pada data X_test
-        predictions = graph.get_tensor_by_name("y_pred:0")  # Ganti nama tensor output
-        predicted_labels = sess.run(
-            predictions, feed_dict={X: combined_feature.reshape(1, 14, 14, 1)}
-        )
+        predictions = graph.get_tensor_by_name("y_pred:0")  # Ganti nama tensor output 
+        predicted_labels = sess.run(predictions, feed_dict={X: combined_feature.reshape(1, 14, 14, 1)})
 
     # predicted_labels sekarang akan berisi hasil prediksi untuk data X_test
     if predicted_labels == 0:
@@ -179,8 +166,7 @@ def try_predict_xception(filepath):
     else:
         return {"gagal mendeteksi label, hasilnya ": predictions}
 
-
-# count
+#count
 def find_majority_vote(items):
     # Count the occurrences of each item in the list
     item_counts = Counter(items)
@@ -194,8 +180,7 @@ def find_majority_vote(items):
     else:
         return "No majority vote"
 
-
-# voting
+#voting
 def voting(filepath):
     hasilV = str(try_predict_vgg(filepath))
     hasilR = str(try_predict_resnet(filepath))
@@ -215,33 +200,28 @@ def voting(filepath):
     else:
         return {"tidak dapat mendeteksi"}
 
-
 # AI code done
-
 
 @app.get("/hello-world")
 async def root():
     return {"message": "Hello World"}
 
-
-# Route to predict if the image is real or deepfake
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    if not (file.filename.endswith(".jpg") or file.filename.endswith(".jpeg")):
-        return {"error": "invalid image type, must be jpg or jpeg"}
+async def predict(file: UploadFile):
+    if file.filename.endswith(".jpg") or file.filename.endswith(".jpeg"):
+        file_location = f"public/{file.filename}"
+        with open(file_location, "wb+") as file_object:
+            file_object.write(file.file.read())
 
-    file_location = f"public/{file.filename}"
-    os.makedirs(os.path.dirname(file_location), exist_ok=True)
-
-    async with aiofiles.open(file_location, "wb") as out_file:
-        content = await file.read()  # Read file content
-        await out_file.write(content)  # Write to file
-
-    return voting(file_location)
-
+        if detect_face(file_location):
+            return voting(file_location)
+        else:
+            return {"error": "Tidak ada wajah yang terdeteksi dalam gambar."}
+    else:
+        {"error": "invalid image type, must be jpg or jpeg"} 
+    return {"Format data tidak valid"}
 
 # Run the server
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
