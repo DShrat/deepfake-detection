d44c229515d1a58b65a971aa29dda5c5adf58f71
D Shrat <dshrat29@gmail.com>
2023-11-12 22:29:36 +0700

    update face recog

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ import cv2
 from imgaug import augmenters as iaa
 import os
 import aiofiles
-import uvicorn
-from uvicorn import run
-
+#tambahan
+from PIL import Image
+import matplotlib.pyplot as plt
+import face_recognition
 
 # webserver code start
 app = FastAPI()
@@ -52,31 +53,40 @@ checkpoint_path_V = "./vgg/my_capsule_network"
 
 
 def detect_face(image_path):
+    # load image
     image = cv2.imread(image_path)
-    face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+
+    # Haar-Cascade face detection
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # read BW
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # cek gambar wajah
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
     if len(faces) == 0:
-        return False  # No faces detected
+        return False  # no faces 
     else:
-        return True  # Faces detected
+        return True  # faces 
 
 
 
 def preprocess_image(img):
-    # Convert the image to a NumPy array
+    # convert image to NumPy 
     img_array = np.array(img)
 
-    # Define augmentation sequence
+    img_array = img_array.astype(np.float32)
+
+    # augmentation
     augmenter = iaa.Sequential([
         iaa.AdditiveGaussianNoise(scale=(0, 0.05 * 255)), 
     ])
 
-    # # Apply the augmentation to the image
+    # apply augmentation
     img_array = augmenter.augment_image(img_array)
 
-    # Resize the augmented image to a specific size (e.g., 224x224 pixels)
+    # resize
     img_array = cv2.resize(img_array, (224, 224))
 
     # Perform any other preprocessing steps like normalization (if needed)
@@ -85,26 +95,42 @@ def preprocess_image(img):
 
 #RESNET50
 def try_predict_resnet(filepath):
-    image = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-    image = np.stack((image, image, image), axis=-1)
+    # image = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    input_image = face_recognition.load_image_file(filepath)
+    #cari wajah
+    face_locations = face_recognition.face_locations(input_image)
+    top, right, bottom, left = face_locations[0]
+    #ekstrak
+    face_image = input_image[top:bottom, left:right]
+    face_image_resized = np.array(Image.fromarray(face_image).resize((224, 224)))
+    face_image_bw = np.mean(face_image_resized, axis=-1, keepdims=True)
+    image = np.concatenate([face_image_bw] * 3, axis=-1)
+
+    # image = np.stack((image, image, image), axis=-1)
     image = preprocess_image(image)
     image = np.expand_dims(image, axis=0)
     image = tf.keras.applications.resnet50.preprocess_input(image)
     features = modelR.predict(image)
     combined_feature = np.mean(features, axis=-1, keepdims=True) #1 layer
+    # image = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    # image = np.stack((image, image, image), axis=-1)
+    # image = preprocess_image(image)
+    # image = np.expand_dims(image, axis=0)
+    # image = tf.keras.applications.resnet50.preprocess_input(image)
+    # features = modelR.predict(image)
+    # combined_feature = np.mean(features, axis=-1, keepdims=True) #1 layer
     
     with tf.compat.v1.Session() as sess:
         saver = tf.compat.v1.train.import_meta_graph(checkpoint_path_R + '.meta')
         saver.restore(sess, checkpoint_path_R)
         graph = tf.compat.v1.get_default_graph()
-        X = graph.get_tensor_by_name("X:0")  # Sesuaikan dengan nama placeholder
+        X = graph.get_tensor_by_name("X:0") 
 
         # prediksi pada data X_test
         predictions = graph.get_tensor_by_name("y_pred:0")  # Ganti nama tensor output 
         predicted_labels = sess.run(predictions, feed_dict={X: combined_feature.reshape(1, 14, 14, 1)})
 
-    # predicted_labels sekarang akan berisi hasil prediksi untuk data X_test
-    # return {str(predicted_labels)}
+    # predicted_labels hasil prediksi X
     if predicted_labels == 0:
         return {"real"}
     elif predicted_labels == 1:
@@ -114,7 +140,17 @@ def try_predict_resnet(filepath):
     
 #VGG19
 def try_predict_vgg(filepath):
-    image = cv2.imread(filepath)
+    # image = cv2.imread(filepath)
+    input_image = face_recognition.load_image_file(filepath)
+    #cari wajah
+    face_locations = face_recognition.face_locations(input_image)
+    top, right, bottom, left = face_locations[0]
+    #ekstrak
+    face_image = input_image[top:bottom, left:right]
+    face_image_resized = np.array(Image.fromarray(face_image).resize((224, 224)))
+    face_image_bw = np.mean(face_image_resized, axis=-1, keepdims=True)
+    image = np.concatenate([face_image_bw] * 3, axis=-1)
+
     image = preprocess_image(image)
     image = np.expand_dims(image, axis=0)
     image = tf.keras.applications.vgg19.preprocess_input(image)
@@ -125,13 +161,13 @@ def try_predict_vgg(filepath):
         saver = tf.compat.v1.train.import_meta_graph(checkpoint_path_V + '.meta')
         saver.restore(sess, checkpoint_path_V)
         graph = tf.compat.v1.get_default_graph()
-        X = graph.get_tensor_by_name("X:0")  # Sesuaikan dengan nama placeholder
+        X = graph.get_tensor_by_name("X:0")  
 
-        # prediksi pada data X_test
-        predictions = graph.get_tensor_by_name("y_pred:0")  # Ganti nama tensor output 
+        # prediksi pada data X
+        predictions = graph.get_tensor_by_name("y_pred:0")   
         predicted_labels = sess.run(predictions, feed_dict={X: combined_feature.reshape(1, 14, 14, 1)})
 
-    # predicted_labels sekarang akan berisi hasil prediksi untuk data X_test
+    # predicted_labels prediksi data X
     if predicted_labels == 0:
         return {"real"}
     elif predicted_labels == 1:
@@ -141,7 +177,17 @@ def try_predict_vgg(filepath):
     
 #XCEPTION
 def try_predict_xception(filepath):
-    image = cv2.imread(filepath)
+    # image = cv2.imread(filepath)
+    input_image = face_recognition.load_image_file(filepath)
+    #cari wajah
+    face_locations = face_recognition.face_locations(input_image)
+    top, right, bottom, left = face_locations[0]
+    #ekstrak
+    face_image = input_image[top:bottom, left:right]
+    face_image_resized = np.array(Image.fromarray(face_image).resize((224, 224)))
+    face_image_bw = np.mean(face_image_resized, axis=-1, keepdims=True)
+    image = np.concatenate([face_image_bw] * 3, axis=-1)
+
     image = preprocess_image(image)
     image = np.expand_dims(image, axis=0)
     image = tf.keras.applications.xception.preprocess_input(image)
@@ -152,13 +198,13 @@ def try_predict_xception(filepath):
         saver = tf.compat.v1.train.import_meta_graph(checkpoint_path_X + '.meta')
         saver.restore(sess, checkpoint_path_X)
         graph = tf.compat.v1.get_default_graph()
-        X = graph.get_tensor_by_name("X:0")  # Sesuaikan dengan nama placeholder
+        X = graph.get_tensor_by_name("X:0")  
 
-        # prediksi pada data X_test
-        predictions = graph.get_tensor_by_name("y_pred:0")  # Ganti nama tensor output 
+        # prediksi pada data X
+        predictions = graph.get_tensor_by_name("y_pred:0")  
         predicted_labels = sess.run(predictions, feed_dict={X: combined_feature.reshape(1, 14, 14, 1)})
 
-    # predicted_labels sekarang akan berisi hasil prediksi untuk data X_test
+    # predicted_labels prediksi data X
     if predicted_labels == 0:
         return {"real"}
     elif predicted_labels == 1:
@@ -168,13 +214,12 @@ def try_predict_xception(filepath):
 
 #count
 def find_majority_vote(items):
-    # Count the occurrences of each item in the list
     item_counts = Counter(items)
 
-    # Find the item with the maximum count
+    # find item with the max count
     majority_item, majority_count = item_counts.most_common(1)[0]
 
-    # Check if the majority count is greater than half the length of the list
+    # cek majority
     if majority_count > len(items) / 2:
         return majority_item
     else:
@@ -206,44 +251,25 @@ def voting(filepath):
 async def root():
     return {"message": "Hello World"}
 
-# @app.post("/predict")
-# async def predict(file: UploadFile):
-#     if file.filename.endswith(".jpg") or file.filename.endswith(".jpeg"):
-#         file_location = f"public/{file.filename}"
-#         with open(file_location, "wb+") as file_object:
-#             file_object.write(file.file.read())
-
-#         if detect_face(file_location):
-#             return voting(file_location)
-#         else:
-#             return {"error": "Tidak ada wajah yang terdeteksi dalam gambar."}
-#     else:
-#         {"error": "invalid image type, must be jpg or jpeg"} 
-#     return {"Format data tidak valid"}
-
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    if (file.filename.endswith(".jpg") or file.filename.endswith(".jpeg")):
-
+async def predict(file: UploadFile):
+    if file.filename.endswith(".jpg") or file.filename.endswith(".jpeg"):
         file_location = f"public/{file.filename}"
-        os.makedirs(os.path.dirname(file_location), exist_ok=True)
+        with open(file_location, "wb+") as file_object:
+            file_object.write(file.file.read())
 
-        async with aiofiles.open(file_location, "wb") as out_file:
-            content = await file.read()  # Read file content
-            await out_file.write(content)  # Write to file
+        # if detect_face(file_location):
+        #     # return voting(file_location)
+        #     return try_predict_resnet(file_location)
+        # else:
+        #     return {"error": "Tidak ada wajah yang terdeteksi dalam gambar."}
+        
+        return voting(file_location)
+        # return try_predict_resnet(file_location)
 
-        # with open(file_location, "wb+") as file_object:
-        #     file_object.write(file.file.read())
-
-        if detect_face(file_location):
-            return voting(file_location)
-        else:
-            return {"error": "Tidak ada wajah yang terdeteksi dalam gambar."}
     else:
         {"error": "invalid image type, must be jpg or jpeg"} 
     return {"Format data tidak valid"}
-
-
 
 # Run the server
 if __name__ == "__main__":
